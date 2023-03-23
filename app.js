ca72dc7d95f7bf5c7441011165189f32c929019f
akane <f23triomphe@gmail.com>
2023-03-23 10:21:29 +0100

    new version

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const textH1 = document.getElementById('animate')
-const message = "Hi 👋 My name is  Forrest, And I am a Web developper "
-const speed = 100
-let i = 0
+// const textH1 = document.getElementById('animate')
+// const message = "Hi 👋 My name is  Forrest, And I am a Web developper "
+// const speed = 100
+// let i = 0
 
-function typeWritter(){
-    if( i < message.length ){
-        textH1.innerHTML += message.charAt(i)
-        i++;
-        setTimeout(typeWritter,speed)
-    }
-}
-typeWritter()
+// function typeWritter(){
+//     if( i < message.length ){
+//         textH1.innerHTML += message.charAt(i)
+//         i++;
+//         setTimeout(typeWritter,speed)
+//     }
+// }
+// typeWritter()
